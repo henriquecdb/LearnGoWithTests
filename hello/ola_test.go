50de622d055126bae0b7681d4bedac7c4b586cc8
Henrique <henriquecamposj@gmail.com>
2024-04-18 12:11:50 -0300

    Fixed directories

---
+++ hello/ola_test.go
@@ -0,0 +1,36 @@
+package hello
+
+import "testing"
+
+func TestOla(t *testing.T) {
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Chris", "ingles")
+		esperado := "Olá, Chris"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("'Mundo' como padrão para 'string' vazia", func(t *testing.T) {
+		resultado := Ola("", "ingles")
+		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em espanhol", func(t *testing.T) {
+		resultado := Ola("Elodie", "espanhol")
+		esperado := "Hola, Elodie"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em frances", func(t *testing.T) {
+		resultado := Ola("Elodie", "frances")
+		esperado := "Bonjour, Elodie"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+}
