3f51c7ca9e8ed304d5190ff54ba66a25e00892d8
Henrique <henriquecamposj@gmail.com>
2024-04-22 18:06:14 -0300

    Still checking Pointers

---
+++ ponteiros/carteira.go
@@ -0,0 +1,15 @@
+package main
+
+type Bitcoin int
+
+type Carteira struct {
+	saldo Bitcoin
+}
+
+func (c *Carteira) Depositar(quantidade Bitcoin) {
+	c.saldo += quantidade
+}
+
+func (c *Carteira) Saldo() Bitcoin {
+	return c.saldo
+}
---
+++ ponteiros/carteira_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCarteira(t *testing.T) {
+	carteira := Carteira{}
+	carteira.Depositar(Bitcoin(10))
+
+	resultado := carteira.Saldo()
+	esperado := Bitcoin(10)
+
+	if resultado != esperado {
+		t.Errorf("resultado %.d, esperado %.d", resultado, esperado)
+	}
+}
