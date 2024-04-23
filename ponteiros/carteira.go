2a1564a5ff21ee418d1b8f99a6832a5092c5d452
Henrique <henriquecamposj@gmail.com>
2024-04-22 21:32:07 -0300

    Finished pointers and errors lesson

--- ponteiros/carteira.go
+++ ponteiros/carteira.go
@@ -1,10 +1,21 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Bitcoin int
 
 type Carteira struct {
 	saldo Bitcoin
 }
+
+type Stringer interface {
+	String() string
+}
+
+var ErroSaldoInsuficiente = errors.New("não é possível retirar: saldo insuficiente")
 
 func (c *Carteira) Depositar(quantidade Bitcoin) {
 	c.saldo += quantidade
@@ -12,4 +23,17 @@ func (c *Carteira) Depositar(quantidade Bitcoin) {
 
 func (c *Carteira) Saldo() Bitcoin {
 	return c.saldo
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (c *Carteira) Retirar(quantidade Bitcoin) error {
+	if quantidade > c.saldo {
+		return ErroSaldoInsuficiente
+	}
+
+	c.saldo -= quantidade
+	return nil
 }
