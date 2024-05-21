6f09de7ba0616e937217439f3244f73cf9c5fe30
Henrique <henriquecamposj@gmail.com>
2024-05-21 15:05:59 -0300

    Finished context lesson

--- context/context.go
+++ context/context.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"context"
+	"fmt"
+	"net/http"
 )
 
 type Store interface {
-  Fetch() string
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, store.Fetch())
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return
+		}
+
+		fmt.Fprint(w, data)
+	}
 }
